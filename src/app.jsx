cf47402c9037da590d7f57eb43aa83ccd358e2a5
geena-keem <geena.gamm@gmail.com>
2021-08-27 00:12:43 +0900

    @fortawesome/fontawesome-free 설치

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
   const name = 'geena';
 
-  return (
-    <>
-      <h1>Hello!</h1>
-      {name && <h1>Hello! {name} :)</h1>}
-
-      {['🎈', '✨'].map((item) => (
-        <h1>{item}</h1>
-      ))}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
