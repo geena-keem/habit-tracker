a3422360e75bc8538411164c1fc3a4261925360b
geena-keem <geena.gamm@gmail.com>
2021-08-29 18:29:38 +0900

    Navbar 컴포넌트 생성 및 css

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    // console.log(`hadleIncrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+
+    habits[index].count++;
+
+    // this.setState({ habits: habits });
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    // console.log(`hadleDecrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+
+    habits[index].count = count < 0 ? 0 : count;
+
+    // if (habits[index].count <= 0) {
+    //   return;
+    // } else {
+    //   habits[index].count--;
+    // }
+
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    // console.log(`handleDelete ${habit.name}`);
+
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    // filter를 사용해서 this.state에 있는 habits을 빙글빙글 돌면서 item을 전달 받은 후
+    // item.id와 삭제하고자 하는 habit.id가 동일하지 않은 것들만 골라내서 새로운 배열로 반환한다.
+
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        {/* totalCount: this.state에 있는 habits배열 중에 
+        item.count가 0이상인 것들만 배열을 새로 만든 후 그 길이를 전달해준다. */}
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,56 +2,22 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    // console.log(`hadleIncrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-
-    habits[index].count++;
-
-    // this.setState({ habits: habits });
-    this.setState({ habits });
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    // console.log(`hadleDecrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-
-    habits[index].count = count < 0 ? 0 : count;
-
-    // if (habits[index].count <= 0) {
-    //   return;
-    // } else {
-    //   habits[index].count--;
-    // }
-
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    // console.log(`handleDelete ${habit.name}`);
-
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    // filter를 사용해서 this.state에 있는 habits을 빙글빙글 돌면서 item을 전달 받은 후
-    // item.id와 삭제하고자 하는 habit.id가 동일하지 않은 것들만 골라내서 새로운 배열로 반환한다.
-
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
 
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
