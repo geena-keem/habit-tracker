3c259e396a274c27c0a4c3720a4bce2aa0f92dfc
geena-keem <geena.gamm@gmail.com>
2021-08-29 19:30:01 +0900

    Reset 버튼 생성 및 구현

--- src/app.jsx
+++ src/app.jsx
@@ -59,6 +59,15 @@ class App extends Component {
     this.setState({ habits });
   };
 
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <>
@@ -73,6 +82,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.HandleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
