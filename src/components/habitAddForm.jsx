7a5c442930a17e643677bf4a82a4f5abe224a8dd
geena-keem <geena.gamm@gmail.com>
2021-09-02 23:07:37 +0900

    function 컴포넌트 + memo

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,42 +1,38 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends PureComponent {
+// memo: PureComponent와 비슷하다
+const HabitAddForm = memo((props) => {
   // PureComponent는 props와 state안에 들어 있는 데이터가 최상위에 있는 데이터가 변하지 않으면
   //                 render함수가 호출되지 않는다. 즉, re-rendering을 하지 않는다!
-  formRef = React.createRef();
-  inputRef = React.createRef();
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
 
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
     // console.log('submit');
     // 페이지가 리로딩되는 이유는 기본적으로 submit이 발생하면
     // 페이지가 리프레시가 되거나 다른 페이지로 가는걸 예상하기 때문이다.
-
     // console.log(this.inputRef.current.value);
 
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
 
     // input입력 후 초기화
     // this.inputRef.current.value = '';
-    this.formRef.current.reset();
+    formRef.current.reset();
   };
-
-  render() {
-    console.log('habitAddForm');
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          className="add-input"
-          type="text"
-          placeholder="Habit"
-        />
-        <button className="add-button">ADD</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        className="add-input"
+        type="text"
+        placeholder="Habit"
+      />
+      <button className="add-button">ADD</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
