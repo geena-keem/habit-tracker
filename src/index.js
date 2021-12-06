dee42faa775cc5bd3847a990627f59142fde22ba
geena-keem <geena.gamm@gmail.com>
2021-12-07 00:23:24 +0900

    add gh-page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
-import SimpleHabit from './components/simpleHabit';
+// import SimpleHabit from './components/simpleHabit';
+// import Habit from './components/habit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SimpleHabit />
+    {/* <SimpleHabit /> */}
+    {/* <Habit /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
